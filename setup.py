a1697ed76536593b3f55b248ce95f6a2af31e03d
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2022-09-02 17:00:58 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,42 +1,57 @@
 #!/usr/bin/env python3
 
 import setuptools
+from pathlib import Path
 
-with open("README.md") as fh:
-    long_description = fh.read()
+long_description = Path("README.md").read_text()
+install_requires = Path("requirements.txt").read_text()
 
-with open("requirements.txt") as fh:
-    install_requires = fh.read()
+package_name = "vsencode"
 
-name = "vsencode"
-version = "1.0.0"
-release = "1.0.0"
+exec(Path(f'{package_name}/_metadata.py').read_text(), meta := dict[str, str]())
 
 setuptools.setup(
-    name=name,
-    version=release,
-    author="LightArrowsEXE",
-    author_email="Lightarrowsreboot@gmail.com",
-    description="vs-encode, an extensive wrapper around vardautomation.",
+    name=package_name,
+    version=meta['__version__'],
+    author=meta['__author_name__'],
+    author_email=meta['__author_email__'],
+    maintainer=meta['__maintainer_name__'],
+    maintainer_email=meta['__maintainer_email__'],
+    description=meta['__doc__'],
     long_description=long_description,
     long_description_content_type="text/markdown",
-    packages=["vsencode", "vsencode.runner", "vsencode.templates"],
-    url="https://github.com/Irrational-Encoding-Wizardry/vs-encode",
+    packages=[
+        package_name
+    ],
     package_data={
-        'vsencode': ['py.typed'],
+        package_name: ['py.typed'],
     },
     install_requires=install_requires,
+    project_urls={
+        "Source Code": 'https://github.com/Irrational-Encoding-Wizardry/vs-encode',
+        "Documentation": 'https://vsencode.encode.moe/en/latest/',
+        "Contact": 'https://discord.gg/qxTxVJGtst',
+    },
     classifiers=[
+        "Natural Language :: English",
+
+        "Intended Audience :: Developers",
+        "Intended Audience :: Other Audience",
+
         "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Typing :: Typed",
+
+        "Topic :: Multimedia :: Video",
+        "Topic :: Multimedia :: Video :: Display",
     ],
     python_requires='>=3.10',
     command_options={
         "build_sphinx": {
-            "project": ("setup.py", name),
-            "version": ("setup.py", version),
-            "release": ("setup.py", release),
+            "project": ("setup.py", package_name),
+            "version": ("setup.py", meta['__version__']),
+            "release": ("setup.py", meta['__version__']),
             "source_dir": ("setup.py", "docs")
         }
     }
