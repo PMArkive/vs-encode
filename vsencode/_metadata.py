aa45ad1447f50203bf848261013e40b317dbd424
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2022-09-02 16:38:51 +0200

    Add package metadata

---
+++ vsencode/_metadata.py
@@ -0,0 +1,13 @@
+"""VapourSynth encoding suite."""
+
+__version__ = '0.0.1'
+
+# TODO: Add multiple authors support
+__author__ = 'LightArrowsEXE <LightArrowsReboot@gmail.com>'
+__maintainer__ = __author__
+
+__author_name__, __author_email__ = [x[:-1] for x in __author__.split('<')]
+__maintainer_name__, __maintainer_email__ = [x[:-1] for x in __maintainer__.split('<')]
+
+if __name__ == '__github__':
+    print(__version__)
